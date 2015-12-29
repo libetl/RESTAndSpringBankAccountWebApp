7cc7117598ae81b7554f98fd5dff0768eda3d5d1
libetl <libe4@free.fr>
2015-12-30 00:46:44 +0100

    start of html support for api

--- src/main/java/org/toilelibre/libe/bank/ioc/webapp/html/HtmlMessageConverter.java
+++ src/main/java/org/toilelibre/libe/bank/ioc/webapp/html/HtmlMessageConverter.java
@@ -23,7 +23,7 @@ import org.toilelibre.libe.bank.actions.entity.PrimitiveNode;
 public class HtmlMessageConverter implements GenericHttpMessageConverter<Object> {
 
     private static final String FOOTER_WITH_GO_BACK_LINK = "<p><a href=\"javascript:history.go(-1)\">Go back</a></p></body></html></body></html>";
-    private static final String HEADER_WITH_DOCTYPE = "<!doctype html><html><head><title>";
+    private static final String HEADER_WITH_DOCTYPE      = "<!doctype html><html><head><title>";
 
     @Override
     public boolean canRead (final Class<?> clazz, final MediaType mediaType) {
@@ -70,14 +70,14 @@ public class HtmlMessageConverter implements GenericHttpMessageConverter<Object>
     @SuppressWarnings ("unchecked")
     @Override
     public void write (final Object t, final Type type, final MediaType contentType, final HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
-        if (!(t instanceof Response)) {
+        if (! (t instanceof Response)) {
             boolean written = false;
             written = this.writeError (t, outputMessage);
             this.writeDefaultResponse (outputMessage, written);
             return;
         }
-        Response<Object> response = (Response<Object>) t;
-        outputMessage.getBody ().write (HEADER_WITH_DOCTYPE.getBytes ());
+        final Response<Object> response = (Response<Object>) t;
+        outputMessage.getBody ().write (HtmlMessageConverter.HEADER_WITH_DOCTYPE.getBytes ());
         outputMessage.getBody ().write (response.getSelf ().getRel ().getBytes ());
         outputMessage.getBody ().write ("</title></head><body><h1>".getBytes ());
         outputMessage.getBody ().write (response.getSelf ().getRel ().getBytes ());
@@ -85,13 +85,12 @@ public class HtmlMessageConverter implements GenericHttpMessageConverter<Object>
         outputMessage.getBody ().write (response.getOk () == 1 ? "<p>Request successful</p>".getBytes () : "<p>Problem during request</p>".getBytes ());
         outputMessage.getBody ().write ("</p><h2>".getBytes ());
         outputMessage.getBody ().write (response.getType ().getBytes ());
-        
-        Node content =  NodeFactory.instance.pojoNode (response.getContent ());
-        
-        ArrayNode links = new ArrayNode ();
-        if (content instanceof ComplexObjectNode && 
-                ((ComplexObjectNode<Node>)content).containsKey ("links")) {
-            links.addAll ((ArrayNode) ((ComplexObjectNode<Node>)content).remove ("links"));
+
+        final Node content = NodeFactory.instance.pojoNode (response.getContent ());
+
+        final ArrayNode links = new ArrayNode ();
+        if ( (content instanceof ComplexObjectNode) && ((ComplexObjectNode<Node>) content).containsKey ("links")) {
+            links.addAll ((ArrayNode) ((ComplexObjectNode<Node>) content).remove ("links"));
         }
         outputMessage.getBody ().write ("</h2><p>".getBytes ());
         this.writeContent (content, outputMessage, links);
@@ -100,81 +99,84 @@ public class HtmlMessageConverter implements GenericHttpMessageConverter<Object>
         outputMessage.getBody ().write ("</p><p><a href=\"javascript:history.go(-1)\">Go back</a></p></body></html>".getBytes ());
     }
 
-    private void writeLinks (ArrayNode links, HttpOutputMessage outputMessage) throws IOException {
-        if (links.size () == 0) {
-            return;
+    private void writeContent (final ArrayNode value, final HttpOutputMessage outputMessage, final ArrayNode links) throws IOException {
+        outputMessage.getBody ().write ("<ol>".getBytes ());
+        if (value.size () == 0) {
+            outputMessage.getBody ().write ( ("<li>Nothing</li>").getBytes ());
         }
-        outputMessage.getBody ().write ("<h2>Links</h2><ul>".getBytes ());
-        for (Node link : links) {
-            @SuppressWarnings ("unchecked")
-            ComplexObjectNode<PrimitiveNode> trueLink = (ComplexObjectNode<PrimitiveNode>) link;
-            outputMessage.getBody ().write (("<li><a href=\"" + trueLink.get ("href").asText () + "\" title=\"" + trueLink.get ("rel").asText () + "\">" +
-                    trueLink.get ("href").asText () + "</a></li>").getBytes ());
+        for (final Node node : value) {
+            outputMessage.getBody ().write ( ("<li>").getBytes ());
+            this.writeContent (node, outputMessage, links);
+            outputMessage.getBody ().write ("</li>".getBytes ());
         }
-        outputMessage.getBody ().write ("</ul>".getBytes ());
+        outputMessage.getBody ().write ("</ol>".getBytes ());
     }
 
-    private void writeContent (ComplexObjectNode<Node> content, HttpOutputMessage outputMessage, ArrayNode links) throws IOException {
+    private void writeContent (final ComplexObjectNode<Node> content, final HttpOutputMessage outputMessage, final ArrayNode links) throws IOException {
         outputMessage.getBody ().write ("<ul>".getBytes ());
 
         if (content.containsKey ("links")) {
             links.addAll ((ArrayNode) (content).remove ("links"));
         }
         if (content.size () == 0) {
-            outputMessage.getBody ().write (("<li>Nothing</li>").getBytes ());
+            outputMessage.getBody ().write ( ("<li>Nothing</li>").getBytes ());
         }
-        for (Entry<String, Node> node : content.entrySet ()) {
-            outputMessage.getBody ().write (("<li>" + node.getKey () + " : ").getBytes ());
+        for (final Entry<String, Node> node : content.entrySet ()) {
+            outputMessage.getBody ().write ( ("<li>" + node.getKey () + " : ").getBytes ());
             this.writeContent (node.getValue (), outputMessage, links);
-            outputMessage.getBody ().write ("</li>".getBytes ()); 
+            outputMessage.getBody ().write ("</li>".getBytes ());
         }
         outputMessage.getBody ().write ("</ul>".getBytes ());
     }
 
     @SuppressWarnings ("unchecked")
-    private void writeContent (Node value, HttpOutputMessage outputMessage, ArrayNode links) throws IOException {
+    private void writeContent (final Node value, final HttpOutputMessage outputMessage, final ArrayNode links) throws IOException {
         if (value instanceof ArrayNode) {
-            this.writeContent (((ArrayNode)value), outputMessage, links);
+            this.writeContent ( ((ArrayNode) value), outputMessage, links);
         }
         if (value instanceof ComplexObjectNode) {
-            this.writeContent (((ComplexObjectNode<Node>)value), outputMessage, links);
+            this.writeContent ( ((ComplexObjectNode<Node>) value), outputMessage, links);
         }
         if (value instanceof PrimitiveNode) {
-            this.writeContent (((PrimitiveNode)value), outputMessage);
+            this.writeContent ( ((PrimitiveNode) value), outputMessage);
         }
     }
 
-    private void writeContent (ArrayNode value, HttpOutputMessage outputMessage, ArrayNode links) throws IOException {
-        outputMessage.getBody ().write ("<ol>".getBytes ());
-        if (value.size () == 0) {
-            outputMessage.getBody ().write (("<li>Nothing</li>").getBytes ());
-        }
-        for (Node node : value) {
-            outputMessage.getBody ().write (("<li>").getBytes ());
-            this.writeContent (node, outputMessage, links);
-            outputMessage.getBody ().write ("</li>".getBytes ()); 
-        }
-        outputMessage.getBody ().write ("</ol>".getBytes ());
+    private void writeContent (final PrimitiveNode value, final HttpOutputMessage outputMessage) throws IOException {
+        outputMessage.getBody ().write ( ("<code>" + value.asText () + "</code>").getBytes ());
     }
 
-    private void writeContent (PrimitiveNode value, HttpOutputMessage outputMessage) throws IOException {
-        outputMessage.getBody ().write (("<code>" + value.asText () + "</code>").getBytes ());
-    }
-
-    private void writeDefaultResponse (HttpOutputMessage outputMessage, boolean alreadyWritten) throws IOException {
+    private void writeDefaultResponse (final HttpOutputMessage outputMessage, final boolean alreadyWritten) throws IOException {
         if (!alreadyWritten) {
-            outputMessage.getBody ().write ((HEADER_WITH_DOCTYPE + "Unrecognized response</title></head><body><h1>Problem</h1><p>Sorry, unrecognized response</p>" + FOOTER_WITH_GO_BACK_LINK).getBytes ());
+            outputMessage.getBody ().write ( (HtmlMessageConverter.HEADER_WITH_DOCTYPE
+                    + "Unrecognized response</title></head><body><h1>Problem</h1><p>Sorry, unrecognized response</p>" + HtmlMessageConverter.FOOTER_WITH_GO_BACK_LINK).getBytes ());
         }
     }
 
-    private boolean writeError (Object t, HttpOutputMessage outputMessage) throws IOException {
-        if (t instanceof ObjectNode && "0".equals (((ObjectNode<?>)t).get ("ok").asText ())) {
-            ObjectNode<?> errorPayload = (ObjectNode<?>) t;
-            outputMessage.getBody ().write (
-                    (HEADER_WITH_DOCTYPE + "Error</title></head><body><h1>" + errorPayload.get ("name").asText () + "</h1><p>The kind of this error is <code>" + 
-                            errorPayload.get ("kind").asText () + "</code></p><p><u>Description :</u> " + errorPayload.get ("description").asText () + "</p>" + FOOTER_WITH_GO_BACK_LINK).getBytes ());            
+    private boolean writeError (final Object t, final HttpOutputMessage outputMessage) throws IOException {
+        if ( (t instanceof ObjectNode) && "0".equals ( ((ObjectNode<?>) t).get ("ok").asText ())) {
+            final ObjectNode<?> errorPayload = (ObjectNode<?>) t;
+            outputMessage.getBody ()
+                    .write ( (HtmlMessageConverter.HEADER_WITH_DOCTYPE + "Error</title></head><body><h1>" + errorPayload.get ("name").asText ()
+                            + "</h1><p>The kind of this error is <code>" + errorPayload.get ("kind").asText () + "</code></p><p><u>Description :</u> "
+                            + errorPayload.get ("description").asText () + "</p>" + HtmlMessageConverter.FOOTER_WITH_GO_BACK_LINK).getBytes ());
             return true;
         }
         return false;
+    }
+
+    private void writeLinks (final ArrayNode links, final HttpOutputMessage outputMessage) throws IOException {
+        if (links.size () == 0) {
+            return;
+        }
+        outputMessage.getBody ().write ("<h2>Links</h2><ul>".getBytes ());
+        for (final Node link : links) {
+            @SuppressWarnings ("unchecked")
+            final ComplexObjectNode<PrimitiveNode> trueLink = (ComplexObjectNode<PrimitiveNode>) link;
+            outputMessage.getBody ().write (
+                    ("<li><a href=\"" + trueLink.get ("href").asText () + "\" title=\"" + trueLink.get ("rel").asText () + "\">" + trueLink.get ("href").asText () + "</a></li>")
+                            .getBytes ());
+        }
+        outputMessage.getBody ().write ("</ul>".getBytes ());
     }
 }
