b9ab17bc25471ee72c5c434db6bcdcba136ced47
LiBe <libetl@users.noreply.github.com>
2016-01-19 11:11:47 +0100

    Start from Java class instead of mvn jetty:run

---
+++ src/main/java/org/toilelibre/libe/bank/ioc/webapp/ServerMain.java
@@ -0,0 +1,29 @@
+package org.toilelibre.libe.bank.ioc.webapp;
+
+import java.util.Arrays;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+
+public class ServerMain {
+
+    public static void main (String [] args) throws Exception
+    {
+    	Logger logbackLogger = (Logger) LoggerFactory.getLogger ("org.eclipse.jetty");
+    	logbackLogger.setLevel (Level.WARN);
+      Server server = new Server (8080);
+
+      WebAppContext webappContext = new WebAppContext ();
+      webappContext.setContextPath ("/");
+      webappContext.getServletContext().setExtendedListenerTypes (true);
+      webappContext.setConfigurationClasses (Arrays.asList (WebAppInitializer.class.getName ()));
+
+      server.setHandler (webappContext);
+      server.start ();
+      server.join ();
+    }
+}
