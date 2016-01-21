f7d0105c0e741781f4ad42509592a8e363837ecb
LiBe <libetl@users.noreply.github.com>
2016-01-21 09:45:35 +0100

    Update ErrorResource.java

--- src/main/java/org/toilelibre/libe/bank/actions/ErrorResource.java
+++ src/main/java/org/toilelibre/libe/bank/actions/ErrorResource.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -52,7 +53,7 @@ public class ErrorResource {
     }
 
     @RequestMapping (path = "badRequest")
-    @ExceptionHandler ({ IllegalArgumentException.class, ServletException.class })
+    @ExceptionHandler ({ IllegalArgumentException.class, ServletException.class, HttpMessageNotReadableException.class })
     @ResponseStatus (code = HttpStatus.BAD_REQUEST)
     public ObjectNode<Node> badRequest (final Exception exception) {
         final NodeFactory factory = NodeFactory.instance;
