850ec9d5b82ceb7106d1532c7a706816de0faeab
libetl <libe4@free.fr>
2018-01-24 22:27:18 +0100

    bootstrap.css location changed

--- src/main/java/org/toilelibre/libe/bank/ioc/webapp/html/HtmlMessageConverter.java
+++ src/main/java/org/toilelibre/libe/bank/ioc/webapp/html/HtmlMessageConverter.java
@@ -36,7 +36,7 @@ public class HtmlMessageConverter implements GenericHttpMessageConverter<Object>
     private static final String HEADER_WITH_DOCTYPE      = "<!doctype html><html><head><script src=\"//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js\"></script>"
             + "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\" "
             + "integrity=\"sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7\" crossorigin=\"anonymous\"/><link rel=\"stylesheet\" href=\""
-            + "https://bootswatch.com/darkly/bootstrap.css\"/><script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\" "
+            + "https://bootswatch.com/4/darkly/bootstrap.css\"/><script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\" "
             + "integrity=\"sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS\" crossorigin=\"anonymous\"></script><title>";
 
     @Override
